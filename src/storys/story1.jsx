5799285aff51605aacd82405953c8ca6a63b8091
John Sanabria <JohnSanabria@gmail.com>
2025-04-04 16:07:11 -0400

    story zustand1

---
+++ src/storys/story1.jsx
@@ -0,0 +1,21 @@
+
+  import { create } from 'zustand'
+
+
+
+const usePokemonStore = create((set) => ({
+    favorites: [],
+    removeFavorite: (pokemonToRemove) =>
+        set((state) => ({
+          favorites: state.favorites.filter(
+            (pokemon) => pokemon.name !== pokemonToRemove.name
+          ),
+        })),
+    addFavorite: (pokemon) =>
+      set((state) => {
+        if (state.favorites.find((fav) => fav.name === pokemon.name))
+          return state;
+        return { favorites: [...state.favorites, pokemon] };
+      }),
+  }));
+  
